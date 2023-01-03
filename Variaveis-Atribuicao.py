10f9daac34279bfb3f2dc8d3fb726acaf0fef846
YanPachecoDEV <97997250+YanPachecoDEV@users.noreply.github.com>
2023-01-02 21:57:12 -0300

    segundo cod

---
+++ Variaveis-Atribuicao.py
@@ -0,0 +1,11 @@
+#VARIAVEIS E ATRIBUIÇÃO A PARTIR DE DADOS INFORMADOS PELO USUÁRIO
+
+nome = input ('Digite seu nome: ')
+cpf = input ('Dgite seu CPF: ')
+endereco = input ('Digite seu endereço: ')
+idade = input ('Digite sua idade: ')
+altura = input ('Digite sua altura: ')
+telefone = input ('Digite seu telefone: ')
+
+print (nome,', portador do CPF:',cpf,', reside na ',endereco,', sua idade é ',
+idade,'. Possui altura: ',altura,'. Telefone para contato ',telefone)
